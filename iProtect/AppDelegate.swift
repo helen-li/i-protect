c5abfd9aa04fea742e501526327fc8b31e2a490e
Helen Li <hallenljy@gmail.com>
2021-07-05 21:25:26 -0400

    Update AppDelegate.swift

--- iProtect/AppDelegate.swift
+++ iProtect/AppDelegate.swift
@@ -1,8 +1,10 @@
 //
-//  AppDelegate.swift
-//  iProtect
+// AppDelegate.swift
+// iProtect
 //
-//  Created by Helen Li on 7/5/21.
+// Created by Helen Li on 7/5/21.
+// Used example code from Aaron Wright's tutorial published on Medium, titled "Create a macOS Menu Bar
+// Application Using SwiftUI."
 //
 
 import Cocoa
@@ -11,29 +13,38 @@ import SwiftUI
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    var window: NSWindow!
-
-
+    var popover: NSPopover!
+    var statusBarItem: NSStatusItem!
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Create the SwiftUI view that provides the window contents.
         let contentView = ContentView()
 
-        // Create the window and set the content view.
-        window = NSWindow(
-            contentRect: NSRect(x: 0, y: 0, width: 480, height: 300),
-            styleMask: [.titled, .closable, .miniaturizable, .resizable, .fullSizeContentView],
-            backing: .buffered, defer: false)
-        window.isReleasedWhenClosed = false
-        window.center()
-        window.setFrameAutosaveName("Main Window")
-        window.contentView = NSHostingView(rootView: contentView)
-        window.makeKeyAndOrderFront(nil)
+        // Create the popover
+        let popover = NSPopover()
+        popover.contentSize = NSSize(width: 400, height: 500)
+        popover.behavior = .transient
+        popover.contentViewController = NSHostingController(rootView: contentView)
+        self.popover = popover
+        
+        // Create the status item
+        self.statusBarItem = NSStatusBar.system.statusItem(withLength: CGFloat(NSStatusItem.variableLength))
+        
+        if let button = self.statusBarItem.button {
+            button.image = NSImage(named: "Icon")
+            button.action = #selector(togglePopover(_:))
+        }
     }
-
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
+    
+    @objc func togglePopover(_ sender: AnyObject?) {
+        if let button = self.statusBarItem.button {
+            if self.popover.isShown {
+                self.popover.performClose(sender)
+            } else {
+                self.popover.show(relativeTo: button.bounds, of: button, preferredEdge: NSRectEdge.minY)
+                self.popover.contentViewController?.view.window?.becomeKey()
+            }
+        }
     }
-
-
+    
 }
-
