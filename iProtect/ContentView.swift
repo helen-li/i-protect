cf5e232e506c7e196f0afbf9f743af3dbbbefb1c
Helen Li <hallenljy@gmail.com>
2021-07-05 21:25:33 -0400

    Update ContentView.swift

--- iProtect/ContentView.swift
+++ iProtect/ContentView.swift
@@ -7,16 +7,48 @@
 
 import SwiftUI
 
+let defaultTimeRemaining: CGFloat = 10
+let lineWith: CGFloat = 30
+let radius: CGFloat = 50
+
 struct ContentView: View {
+    
+    @State private var isActive = false
+    @State private var timeRemaining: CGFloat = defaultTimeRemaining
+    
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
     var body: some View {
-        Text("Hello, World!")
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
+        VStack {
+            ZStack {
+                Circle()
+                    .stroke(Color.gray.opacity(0.2),
+                            style: StrokeStyle(lineWidth: lineWith, lineCap: .round))
+                
+                Circle()
+                    .stroke(Color.green,
+                            style: StrokeStyle(lineWidth: lineWith, lineCap: .round))
+                    .rotationEffect(.degrees(-90))
+                    .animation(.easeInOut)
+                
+                Text("\(timeRemaining)")
+            }.frame(width: radius * 8, height: radius * 8)
+            
+            Button("Start") {
+                isActive.toggle()
+            }
+            
+            if isActive {
+                
+            }
+            
+        }
     }
 }
-
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
